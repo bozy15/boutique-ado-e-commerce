d8b33793d2fe5ea68d35ebf0b89ba0e65b37387b
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-12-03 13:04:55 +0000

    Finished shopping bag logic

--- bag/views.py
+++ bag/views.py
@@ -17,15 +17,35 @@ def add_to_bag(request, item_id):
     # get the url to redirect to from the form
     redirect_url = request.POST.get("redirect_url")
 
+    size = None
+    # check if the user selected a size, if so set the size to what was selected
+    if "product_size" in request.POST:
+        size = request.POST["product_size"]
+
     # get the bag from the session, if it doesn't exist, create an empty dictionary
     bag = request.session.get("bag", {})
-
-    # if the item is already in the bag, add the new quantity to the existing quantity
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    # if the item is not in the bag, add it to the bag with the quantity specified
+    # Check if size has been added
+    if size:
+        if item_id in list(
+            bag.keys()
+        ):  # if the item is already in the bag, add the new size to the existing quantity
+            if size in bag[item_id]["items_by_size"].keys():
+                bag[item_id]["items_by_size"][
+                    size
+                ] += quantity  # update the quantity of the size
+            else:
+                bag[item_id]["items_by_size"][
+                    size
+                ] = quantity  # add the size to the bag
+        else:
+            bag[item_id] = {"items_by_size": {size: quantity}}
     else:
-        bag[item_id] = quantity
+        # if the item is already in the bag, add the new quantity to the existing quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        # if the item is not in the bag, add it to the bag with the quantity specified
+        else:
+            bag[item_id] = quantity
 
-    request.session["bag"] = bag # save the bag back to the session
-    return redirect(redirect_url) # redirect to the url specified in the form
+    request.session["bag"] = bag  # save the bag back to the session
+    return redirect(redirect_url)  # redirect to the url specified in the form
